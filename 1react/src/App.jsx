7e0ce8362edc5edb300965578f9ec18678aa9a8d
thpixel-dev <gamernoob1673@gmail.com>
2024-08-19 17:08:18 -0300

    arquivos adicionados 1react

---
+++ 1react/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1 className='titulo_principal'>hello react js fofinho</h1>
+     <p>lorem knfhe voce , assim . kflekade </p>
+    </>
+  )
+}
+
+export default App
